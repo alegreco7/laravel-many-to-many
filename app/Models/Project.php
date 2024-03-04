0bc9445c9ddc0adc4c771ef8131697eb859a5c68
alegreco7 <alexgreco2002@gmail.com>
2024-03-04 14:16:22 +0100

    checkbox per aggiungere tecnologie al progetto

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,8 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
-    protected $fillable = ['name', 'description', 'start_date', 'end_date', 'slug', 'img', 'type_id'];
+    protected $fillable = ['name', 'description', 'start_date', 'end_date', 'slug', 'img', 'type_id', 'checks'];
     public function type(){
         return $this->belongsTo(Type::class);
+    }
+    public function technologies(){
+        return $this->belongsToMany(Technology::class);
     }
 }
